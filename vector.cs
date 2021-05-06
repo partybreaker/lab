81afde93c4ffc0493220c4f650eeab2929c85738
Tio Kirill <tio.kirill@gmail.com>
2021-05-06 15:44:14 +0300

    допилил векторы

--- vector.cs
+++ vector.cs
@@ -68,7 +68,55 @@ namespace ConsoleApp
             double zz2 = Convert.ToDouble(Console.ReadKey());
             // Ввод данных
             Vector y1 = new Vector(xx2 - xx1, yy1 - yy2, zz1 - zz2);
-
+            Vector v2 = new Vector(xx22 - xx2, yy22 - yy2, yy22 - yy2);
+            Vector v3 = v1 + v2;
+            Vector v4 = v1 - v2;
+            double m = v1 * v2;
+            double c = Vector.Cos(v1, v2);
+            double len1 = v1.Length();
+            double len2 = v2.Length();
+            int test = 0;
+            // Добавим обработку ошибок
+            try
+            {
+                len1 = v1.Length();
+                len2 = v2.Length();
+ 
+                if (len1 == 0 || len2 == 0)
+                {
+                    throw new Exception("Вектор не может быть нулевым");
+                
+                }
+            }
+            catch (Exception e)
+                {
+                    Console.WriteLine("\nОшибка: " + e.Message);
+                    test = 10;
+                }
+            // Вывод данных на экран
+            if (test != 10)
+                {
+                    Console.WriteLine();
+                    Console.Write("Вектор a: ");
+                    Console.WriteLine(v1);
+                    Console.Write("Вектор б: ");
+                    Console.WriteLine(v2);
+                    Console.Write("Вектор а+б: ");
+                    Console.WriteLine(v3);
+                    Console.Write("Вектор а-б: ");
+                    Console.WriteLine(v4);
+                    Console.Write("Вектор а*б: ");
+                    Console.WriteLine(m);
+                    Console.Write("Косинус угла между векторами a и б: ");
+                    Console.WriteLine(c);
+                    Console.Write("Длина вектора 1: ");
+                    Console.WriteLine(len1);
+                    Console.Write("Длина вектора 2: ");
+                    Console.WriteLine(len2);
+                }
+        Console.ReadKey();
+ 
+    }        
 
         }
     }
