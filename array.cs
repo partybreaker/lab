fdd86e7c814a1138736816efbc2cb3aa86cf4dcf
Tio Kirill <tio.kirill@gmail.com>
2021-05-06 15:38:52 +0300

    коменты

--- array.cs
+++ array.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp
         private static int Min(params int[] array){
             int min = array[0]; // temp переменная
             for (int i = 1; i < array.Length; i++){ // проход по массиву
-                min = Math.Min(min, array[i]); // нахождение минимального элемента используя класс Math
+                min = Math.Min(min, array[i]); // нахождение минимального элемента используя класс Math про его методы можно прочитать тут - https://docs.microsoft.com/en-us/dotnet/api/system.math?view=net-5.0
             }
             return min; // возвращает минимальный элемент
         }
