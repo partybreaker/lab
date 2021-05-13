7bfb61f45178d638570e0d12dc31add2e3eebde1
Kirill <tio.kirill@gmail.com>
2021-05-13 15:30:21 +0300

    Update Program.cs

--- StreamExample/StreamExample/Program.cs
+++ StreamExample/StreamExample/Program.cs
@@ -9,13 +9,17 @@ namespace StreamExample
         {
             string [] array;
             Example example = new Example(); // Инициализация экземпляра класса Example (см. файл в проекте Example.cs)
+            /*
+            * Документация FileStream - https://docs.microsoft.com/en-us/dotnet/api/system.io.filestream?view=net-5.0
+            */
             FileStream file = new FileStream("test.txt", FileMode.Create); // Инициализируем FileStream и создаем и указываем имя файла
             /*
-             * Подобное действие производим над бинарными файлами используя BinaryWritter
-             * BinaryWritter binaryW = new BinaryWritter(file);
+             * Документация BinaryWriter - https://docs.microsoft.com/en-us/dotnet/api/system.io.binarywriter?view=net-5.0
+             * Подобное действие производим над бинарными файлами используя BinaryWriter
+             * BinaryWriter binaryW = new BinaryWriter(file);
              * binaryW(exmaple.GetSomeValue(20, 1, 30, 50);
              */
-            StreamWriter write = new StreamWriter(file); // Инициализируем StreamWritter https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?view=net-5.0
+            StreamWriter write = new StreamWriter(file); // Инициализируем StreamWriter https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?view=net-5.0
             write.WriteLine(example.GetSomeValue(20, 1, 30, 40)); // Передаем значения
             /*
              * Если у нас речь о PatientList
@@ -25,7 +29,7 @@ namespace StreamExample
              *      write.Close();
              * }
              */
-            write.Close(); // Закрываем StreamWritter !!!
+            write.Close(); // Закрываем StreamWriter !!!
             file.Close(); // Закрываем FileStream !!
             Console.WriteLine("Все ОК!");
             Console.ReadKey();
