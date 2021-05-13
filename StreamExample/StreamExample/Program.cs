d37ce0797b6da83230e4c4ba9a3c8f682b88099e
Tio Kirill <tio.kirill@gmail.com>
2021-05-13 15:15:08 +0300

    поправил чутка

--- StreamExample/StreamExample/Program.cs
+++ StreamExample/StreamExample/Program.cs
@@ -9,14 +9,13 @@ namespace StreamExample
         {
             string [] array;
             Example example = new Example(); // Инициализация жкхемпляра класса
-            FileStream file = new FileStream("test.txt", FileMode.Create); // Создаем и указываем имя файла
+            FileStream file = new FileStream("test.txt", FileMode.Create); // Инициализируем FileStream и создаем и указываем имя файла
             StreamWriter write = new StreamWriter(file); // Инициализируем StreamWritter https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?view=net-5.0
-            write.WriteLine(example.GetSomeValue(20, 1,30,40));
-            write.Close();
-            file.Close();
+            write.WriteLine(example.GetSomeValue(20, 1, 30, 40)); // Передаем массив на запись
+            write.Close(); // Закрываем StreamWritter !!!
+            file.Close(); // Закрываем FileStream !!
             Console.WriteLine("Все ОК!");
             Console.ReadKey();
-
         }
         }
 }
