c9c2acf2a1484784b0918ccfbd1d6c3c2852155c
Tio Kirill <tio.kirill@gmail.com>
2021-05-13 15:22:25 +0300

    правочки

--- StreamExample/StreamExample/Program.cs
+++ StreamExample/StreamExample/Program.cs
@@ -10,8 +10,21 @@ namespace StreamExample
             string [] array;
             Example example = new Example(); // Инициализация жкхемпляра класса
             FileStream file = new FileStream("test.txt", FileMode.Create); // Инициализируем FileStream и создаем и указываем имя файла
+            /*
+             * Подобное действие производим над бинарными файлами используя BinaryWritter
+             * BinaryWritter binaryW = new BinaryWritter(file);
+             * binaryW(exmaple.GetSomeValue(20, 1, 30, 50);
+             */
             StreamWriter write = new StreamWriter(file); // Инициализируем StreamWritter https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?view=net-5.0
-            write.WriteLine(example.GetSomeValue(20, 1, 30, 40)); // Передаем массив на запись
+            write.WriteLine(example.GetSomeValue(20, 1, 30, 40)); // Передаем значения
+            /*
+             * Если у нас речь о PatientList
+             * foreach(PatientList patient in patients)
+             * {
+             *      write.WriteLine(patient);
+             *      write.Close();
+             * }
+             */
             write.Close(); // Закрываем StreamWritter !!!
             file.Close(); // Закрываем FileStream !!
             Console.WriteLine("Все ОК!");
