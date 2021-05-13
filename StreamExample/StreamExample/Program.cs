3310564c051ecd10462e04f717001c3484239e3a
Kirill <tio.kirill@gmail.com>
2021-05-13 15:25:25 +0300

    Update Program.cs

--- StreamExample/StreamExample/Program.cs
+++ StreamExample/StreamExample/Program.cs
@@ -8,7 +8,7 @@ namespace StreamExample
         public static void Main(string[] args)
         {
             string [] array;
-            Example example = new Example(); // Инициализация экземпляра класса Example (см. файл Example.cs)
+            Example example = new Example(); // Инициализация экземпляра класса Example (см. файл в проекте Example.cs)
             FileStream file = new FileStream("test.txt", FileMode.Create); // Инициализируем FileStream и создаем и указываем имя файла
             /*
              * Подобное действие производим над бинарными файлами используя BinaryWritter
